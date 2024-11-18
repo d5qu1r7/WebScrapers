f2a96fe98ba6892a7161fedaa95f5b8104b57be5
Davis Holdstock <112665636+d5qu1r7@users.noreply.github.com>
2024-11-18 16:33:50 -0700

    Update (HESLB_2016.py) remove unnecessary code

--- Africa/TZ/HESLB_2016.py
+++ Africa/TZ/HESLB_2016.py
@@ -23,48 +23,6 @@ log_file_path = os.path.join(SAVE_FILE_PATH, 'TZ_HESLB_2016.log')
 logging.basicConfig(filename=log_file_path, level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-def separate_tables(text, output_folder):
-    tables = re.split(r"\x0c", text)  # Splitting based on multiple newlines, adjust if needed
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    for i, table in enumerate(tables):
-        lines = table.strip().split("\n")
-        if i == 0:
-            continue
-        elif i == len(tables) - 1:
-            lines = lines[:-1]
-        output_file = os.path.join(output_folder, f'table_{i}.csv')
-        with open(output_file, 'w') as file:
-            for j, line in enumerate(lines):
-                line = line.replace('\r', '')
-                line = line.replace('\n', '')
-                if j == 0:
-                    line = line.replace(', ', '-')
-                    line = re.sub(r'\s\s+', ',', line)
-                elif j == 2:
-                    line = re.sub(r'\s\s+', '', line)
-                elif j > 5 and j < 8:
-                    line = re.sub(r'\s+=\s+', '==', line)
-                    line = re.sub(r'==', ',', line)
-                    line = re.sub(r'\s\s+', '', line)
-                elif j > 10:
-                    pattern = r'^\s+$'
-                    pattern_2 = r'^(\s+)([A-Za-z0-9])(.+)'
-                    
-                    # Use re.match to check if the pattern matches the text 
-                    match = re.match(pattern, line)
-                    match_2 = re.match(pattern_2, line)
-                    if match: 
-                        continue
-                    elif match_2:
-                        all_groups_except_first = match_2.groups()[1:]
-                        line = ''.join(all_groups_except_first)
-                    line = re.sub(r'\s\s+', ',', line)
-                else:
-                    line = re.sub(r'\s\s+', '', line)
-                file.write(line + "\n")
-
 @timer
 def main():
     global BASE_URL, SAVE_FILE_PATH
@@ -84,8 +42,6 @@ def main():
             # Adding everything to a row and writing to .csv
             row_data.append(column.text.strip().replace("\n", " ").replace(",", " "))
         write_row_to_csv(row_data, SAVE_FILE_PATH, 'data')
-
-
 
 if __name__ == "__main__":
     main()
